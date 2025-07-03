22d01d22504c86cb56dbe8704362c03aeb74603d
Devanshu Sharma <devanshusharma658@gmail.com>
2025-07-03 20:06:51 +0530

    Refactor login page and implement LoginForm component for GitHub authentication

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,64 +1,19 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { authClient } from "@/lib/auth-client";
-import { GithubIcon, Loader } from "lucide-react";
-import { useTransition } from "react";
-import { toast } from "sonner";
+import { headers } from "next/headers";
+import { LoginForm } from "./_components/loginForm";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
 
-  const [githubPending, startGithubTransition] = useTransition();
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-  async function signInWithGithub() {
-    startGithubTransition(async () => {
-      await authClient.signIn.social({
-      provider : "github",
-      callbackURL: "/",
-      fetchOptions: {
-        onSuccess: () => {
-          toast.success("Signed in with Github successfully, redirecting...");
-        },
-        onError: (error) => {
-          toast.error("Internal server error, please try again later.");
-        }
-      },
-    });
-    })
+  if(session) {
+    return redirect("/"); // Redirect to home if already logged in
   }
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-xl">Welcome back!</CardTitle>
-        <CardDescription>Login with your Github Email Account</CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-4">
-        <Button 
-        disabled={githubPending}
-        onClick={signInWithGithub}  className="w-full" variant="outline">
-         {githubPending ? (
-          <>
-          <Loader className="size-4 animate-spin"/>
-          <span>Loading...</span>
-          </>
-         ) : (<>
-          <GithubIcon className="size-4"/>
-          <span>Sign in with Github</span>
-         </>)}
-        </Button>
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-card px-2 text-muted-foreground">Or Continue with</span>
-        </div>
-        <div className="grid gap-3">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" placeholder="abc@example.com"/>
-          </div>
-          <Button>Continue with Email</Button>
-        </div>
-      </CardContent>
-    </Card>
+    <LoginForm />
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const { data: session } = authClient.useSession();
+  const router = useRouter()
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login");
+          toast.success("Logged out successfully");
+        },
+      },
+    });
+  }
+
   return (
     <div>
       <h1 className="text-2xl font-bold text-red-500">Hello world</h1>
-      <ThemeToggle/>
+      <ThemeToggle />
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   );
 }
