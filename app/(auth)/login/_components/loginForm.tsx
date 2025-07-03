d95489fefc027ca8a04d72d4010928621a71a1a3
Devanshu Sharma <devanshusharma658@gmail.com>
2025-07-03 21:57:08 +0530

    Otp verification in progress.

--- app/(auth)/login/_components/loginForm.tsx
+++ app/(auth)/login/_components/loginForm.tsx
@@ -4,13 +4,16 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { authClient } from "@/lib/auth-client";
-import { GithubIcon, Loader } from "lucide-react";
-import { useTransition } from "react";
+import { GithubIcon, Loader, Loader2, Send } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
 import { toast } from "sonner";
 
 export function LoginForm() {
-
+  const router = useRouter();
    const [githubPending, startGithubTransition] = useTransition();
+   const [emailPending, startEmailTransition] = useTransition();
+   const [email, setEmail] = useState("");
   
     async function signInWithGithub() {
       startGithubTransition(async () => {
@@ -26,13 +29,34 @@ export function LoginForm() {
           }
         },
       });
+      });
+    }
+
+    function signInWithEmail() {
+      startEmailTransition(async () => {
+        await authClient.emailOtp.sendVerificationOtp({
+          email: email,
+          type: 'sign-in',
+          fetchOptions: {
+            onSuccess: () => {
+              toast.success("Verification email sent, please check your inbox.");
+              router.push(`/verify-request`);
+            },
+            onError: (error) => {
+              toast.error("Failed to send verification email, please try again.");
+            }
+          }
+        })
       })
     }
+
+
+
   return (
      <Card>
       <CardHeader>
         <CardTitle className="text-xl">Welcome back!</CardTitle>
-        <CardDescription>Login with your Github Email Account</CardDescription>
+        <CardDescription>Login with your Github or Email Account</CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col gap-4">
         <Button 
@@ -54,9 +78,22 @@ export function LoginForm() {
         <div className="grid gap-3">
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
-            <Input type="email" placeholder="abc@example.com"/>
+            <Input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="abc@example.com"
+            required/>
           </div>
-          <Button>Continue with Email</Button>
+          <Button onClick={signInWithEmail} disabled={emailPending}>
+            {emailPending ? (
+              <>
+                <Loader2 className="size-4 animate-spin" />
+                <span>Sending...</span>
+              </>
+            ) : (
+              <>
+              <Send className="size-4" />
+              <span>Continue with Email</span>
+              </>
+            )}
+          </Button>
         </div>
       </CardContent>
     </Card>
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,11 @@
+
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { emailOTP } from "better-auth/plugins"
 import { prisma } from "./db";
 import { env } from "./env";
- 
+import { resend } from "./resend";
+
 export const auth = betterAuth({
      database: prismaAdapter(prisma, {
         provider: "postgresql",
@@ -13,6 +16,19 @@ export const auth = betterAuth({
         clientSecret: env.AUTH_GITHUB_SECRET,
       },
     },
+    
+    plugins: [
+        emailOTP({
+            async sendVerificationOTP({ email, otp }){
+               await resend.emails.send({
+               from: 'Lymsy <onboarding@resend.dev>',
+                to: [email],
+                subject: 'Lymsy - Email Verification',
+                html: `<p>Your OTP is <strong>${otp}</strong></p>`
+              });
+            }
+        }),
+    ],
 });
 
 
